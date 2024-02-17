26ef3d59dfc0ae4e8fd09e7e7dde9635a4a993fa
danillo95 <140861435+danillo95@users.noreply.github.com>
2024-02-16 22:31:04 -0300

    atividade1

---
+++ Java/ifruit/src/Product.java
@@ -0,0 +1,19 @@
+public class Product {
+    private String name;
+    private String description;
+    private Double weight;
+    private Double price;
+
+    public void setName(String productName){
+      name = productName;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setDescription(String descriptionProduct){
+        description = descriptionProduct;
+    }
+    public String getDescription(){
+        return description;
+
+}
---
+++ Java/ifruit/src/User.java
@@ -0,0 +1,29 @@
+public class User {
+
+    private String name;
+    private String email;
+    private Integer idade;
+
+    public void pagar(){
+
+    }
+    public void setName(String userName){
+        name = userName;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setEmail(String userEmail){
+        email = userEmail;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public Integer setIdade(Integer userIdade){
+        idade = userIdade;
+    }
+    public Integer getIdade(){
+        return idade;
+    }
+    }
+
